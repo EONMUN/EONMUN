d54d5e468d80bb9d2f9d5a494a38f853a2e1d791
Nicholas Romero <ncrmro@gmail.com>
2025-07-23 15:21:07 -0500

    chore: disable web3 for now

--- web/src/context/index.tsx
+++ web/src/context/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { PropsWithChildren } from 'react'
-import { Web3Provider } from './Web3'
-import { DataProvider } from './Data'
+// import { Web3Provider } from './Web3'
+// import { DataProvider } from './Data'
 // import { NotificationProvider } from './Notifications'
 
 interface Props extends PropsWithChildren {
@@ -12,13 +12,13 @@ interface Props extends PropsWithChildren {
 export function Providers(props: Props) {
   return (
     <>
-      <Web3Provider cookies={props.cookies}>
-        <DataProvider>
+      {/* <Web3Provider cookies={props.cookies}> */}
+        {/* <DataProvider> */}
           {/* <NotificationProvider> */}
             {props.children}
             {/* </NotificationProvider> */}
-        </DataProvider>
-      </Web3Provider>
+        {/* </DataProvider> */}
+      {/* </Web3Provider> */}
     </>
   )
 }
